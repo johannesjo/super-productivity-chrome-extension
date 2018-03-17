f1601c2a14477b1a9a86ba0afbeacd444cf5d067
Johannes Millan <johannes.millan@gmail.com>
2018-03-17 18:51:48 +0100

    feat: make basic initialization work

--- frontend-interface.js
+++ frontend-interface.js
@@ -1,9 +1,20 @@
-function initInterface() {
-  return 'INTERFACE YEAH FE'
+function onInterfaceReady() {
+  console.log('FR ON_INTER');
+
+  chrome.runtime.sendMessage({
+    action: 'interfaceReady',
+    source: 'WHATEVER'
+  });
+
+  window.addEventListener('SP_JIRA_REQUEST', (ev) => {
+    chrome.runtime.sendMessage({
+      action: 'jirRequest',
+      source: ['WHATEVER', 'MORE']
+    });
+  });
 }
 
-chrome.runtime.sendMessage({
-  action: 'initInterface',
-  source: initInterface()
-});
+window.addEventListener('APP_READY', onInterfaceReady);
 
+// set permanent info for frontend
+window.localStorage.setItem('SUPER_PRODUCTIVITY_CHROME_EXTENSION', 'IS_ENABLED');
--- main.js
+++ main.js
@@ -1,9 +1,8 @@
 const action = 'search';
 const base = 'https://test-sp-app.atlassian.net/rest/api/2';
 
-function handleRequest(tab) {
+function handleJiraRequest(tab) {
   console.log(tab);
-
   const xhr = new XMLHttpRequest();
   xhr.open('GET', `${base}/${action}`, true);
   xhr.onload = () => {
@@ -22,36 +21,56 @@ function handleRequest(tab) {
   xhr.send(null);
 }
 
-chrome.browserAction.onClicked.addListener((tab) => {
-  handleRequest(tab);
-});
+function initInterface(tabId) {
+  chrome.tabs.executeScript(tabId, {
+    file: 'frontend-interface.js',
+    allFrames: true,
+    runAt: 'document_idle'
+  }, () => {
+    // If you try and inject into an extensions page or the webstore/NTP you'll get an error
+    if (chrome.runtime.lastError) {
+      console.error(chrome.runtime.lastError);
+    }
+  });
+}
 
-chrome.runtime.onMessage.addListener(function(request) {
-  if (request.action === 'initInterface') {
-    console.log('BE INTERFACE YEAH');
-  }
-});
-
-const SP_URL = 'http://localhost';
-
+// HELPER
+// ------------------------------------
 function isSpUrl(url) {
   return url.startsWith(SP_URL);
 }
-
-const filters = {
-  url: [{ urlContains: SP_URL.replace(/^https?\:\/\//, '') }]
-};
 
 function onNavigate(details) {
   if (details.url && isSpUrl(details.url)) {
     console.log('Recognized SP navigation to: ' + details.url + '.' +
       'Refreshing count...');
-    startRequest({ scheduleRequest: false, showLoadingAnimation: false });
+    initInterface();
   }
 }
 
+// LISTENER
+// ------------------------------------
+chrome.browserAction.onClicked.addListener((tab) => {
+  handleJiraRequest(tab);
+});
+
+chrome.runtime.onMessage.addListener(function(request) {
+  console.log(request);
+  if (request.action === 'interfaceReady') {
+    console.log('BE INTERFACE YEAH');
+  }
+});
+
+// INIT INTERFACE STUFF
+// ------------------------------------
+const SP_URL = 'http://localhost';
+
+// also init when url was entered later
 if (chrome.webNavigation && chrome.webNavigation.onDOMContentLoaded &&
   chrome.webNavigation.onReferenceFragmentUpdated) {
+  const filters = {
+    url: [{ urlContains: SP_URL.replace(/^https?\:\/\//, '') }]
+  };
   chrome.webNavigation.onDOMContentLoaded.addListener(onNavigate, filters);
   chrome.webNavigation.onReferenceFragmentUpdated.addListener(
     onNavigate, filters);
@@ -61,17 +80,10 @@ if (chrome.webNavigation && chrome.webNavigation.onDOMContentLoaded &&
   });
 }
 
-initInterface();
-
-function initInterface() {
-  chrome.tabs.executeScript(null, {
-    file: 'frontend-interface.js',
-    allFrames: true,
-    runAt: 'document_idle'
-  }, () => {
-// If you try and inject into an extensions page or the webstore/NTP you'll get an error
-    if (chrome.runtime.lastError) {
-      console.error(chrome.runtime.lastError);
-    }
-  });
-}
+chrome.tabs.query({
+  url: SP_URL + '/*'
+}, (tabs) => {
+  if (tabs) {
+    initInterface(tabs[0].id);
+  }
+});
