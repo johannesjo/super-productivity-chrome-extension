759ac672415a9441c87a702e1c4cdadc4418126f
Johannes Millan <johannes.millan@gmail.com>
2018-03-18 00:23:22 +0100

    feat: add better error handling

--- src/js/jira.js
+++ src/js/jira.js
@@ -2,6 +2,16 @@ import xhr from 'xhr';
 
 const base = 'https://test-sp-app.atlassian.net/rest/api/2';
 
+function b64EncodeUnicode(str) {
+  // first we use encodeURIComponent to get percent-encoded UTF-8,
+  // then we convert the percent encodings into raw bytes which
+  // can be fed into btoa.
+  return btoa(encodeURIComponent(str).replace(/%([0-9A-F]{2})/g,
+    function toSolidBytes(match, p1) {
+      return String.fromCharCode('0x' + p1);
+    }));
+}
+
 export class JiraApiWrapper {
   constructor() {
     this.xhr = xhr;
@@ -14,12 +24,15 @@ export class JiraApiWrapper {
   }
 
   doRequest(orgRequest, request) {
+    console.log(request);
+
     return new Promise((resolve) => {
       this.xhr({
         uri: `${base}/${request.pathname}`,
         method: 'GET',
         body: request.body,
         headers: {
+          'Authentication': b64EncodeUnicode(`${orgRequest.config.userName}:${orgRequest.config.password}`),
           'Content-Type': 'application/json'
         }
       }, function(err, res, body) {
@@ -29,11 +42,19 @@ export class JiraApiWrapper {
             requestId: orgRequest.requestId
           });
         } else if (body) {
-          //console.log(JSON.parse(body), request, res);
-          resolve({
-            response: JSON.parse(body),
-            requestId: orgRequest.requestId
-          });
+          const parsed = JSON.parse(body);
+          const err = parsed.errorMessages || parsed.errors;
+          if (err) {
+            resolve({
+              error: err,
+              requestId: orgRequest.requestId
+            });
+          } else {
+            resolve({
+              response: parsed,
+              requestId: orgRequest.requestId
+            });
+          }
         }
       });
     });
@@ -64,7 +85,7 @@ export class JiraApiWrapper {
     const issueUpdate = orgRequest.arguments[1];
 
     return this.doRequest(orgRequest, {
-      pathname: `/issue/${issueId}`,
+      pathname: `issue/${issueId}`,
       method: 'PUT',
       body: issueUpdate,
     });
