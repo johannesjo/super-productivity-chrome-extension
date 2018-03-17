2c37c6f2a1e1cafafe931c8acbfd6a6f4850ccd0
Johannes Millan <johannes.millan@gmail.com>
2018-03-17 23:29:10 +0100

    feat: update icon

--- src/js/background.js
+++ src/js/background.js
@@ -1,5 +1,5 @@
-import '../img/icon-128.png'
-import '../img/icon-34.png'
+import '../img/icon_32x32.png'
+import '../img/icon_128x128.png'
 import { JiraApiWrapper } from './jira';
 
 const jira = new JiraApiWrapper();
