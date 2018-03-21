aef6b8104268e8977b7f83d84e2f2237045a455b
Johannes Millan <johannes.millan@gmail.com>
2018-03-21 22:51:20 +0100

    feat: always build for production

--- utils/build.js
+++ utils/build.js
@@ -1,9 +1,14 @@
 var webpack = require("webpack"),
-    config = require("../webpack.config");
+  config = require("../webpack.config");
 
 delete config.chromeExtensionBoilerplate;
 
+// always build for production
+process.env.NODE_ENV = 'production';
+
 webpack(
   config,
-  function (err) { if (err) throw err; }
+  function(err) {
+    if (err) throw err;
+  }
 );
