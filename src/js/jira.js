697d6a13e9d0042c2e656b48ba180cf97be647b4
Johannes Millan <johannes.millan@gmail.com>
2018-12-09 23:26:50 +0100

    Revert "feat: cleanup uneeded depdency"
    This reverts commit d7baf19f

--- src/js/jira.js
+++ src/js/jira.js
@@ -1,8 +1,10 @@
 import xhr from 'xhr';
+import queryStringParser from 'query-string';
 
 export class JiraApiWrapper {
   constructor() {
     this.xhr = xhr;
+    this.queryStringParser = queryStringParser.stringify;
   }
 
   isConfigSufficient(config) {
